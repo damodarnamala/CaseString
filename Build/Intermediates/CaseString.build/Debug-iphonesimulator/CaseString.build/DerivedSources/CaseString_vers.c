7f8f8d7d92d0c1ccdf6626e1e95ad224ddd37a5a
Damodar Namala <damodarnamala@damodars-imac.local>
2016-12-30 12:22:22 +0530

    Initial Commit

---
+++ Build/Intermediates/CaseString.build/Debug-iphonesimulator/CaseString.build/DerivedSources/CaseString_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char CaseStringVersionString[];
+ extern const double CaseStringVersionNumber;
+
+ const unsigned char CaseStringVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CaseString  PROJECT:CaseString-1" "\n";
+ const double CaseStringVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ CaseString/CaseString.h
@@ -0,0 +1,19 @@
+//
+//  CaseString.h
+//  CaseString
+//
+//  Created by Damodar Namala on 30/12/16.
+//  Copyright © 2016 Damodar Namala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CaseString.
+FOUNDATION_EXPORT double CaseStringVersionNumber;
+
+//! Project version string for CaseString.
+FOUNDATION_EXPORT const unsigned char CaseStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaseString/PublicHeader.h>
+
+
